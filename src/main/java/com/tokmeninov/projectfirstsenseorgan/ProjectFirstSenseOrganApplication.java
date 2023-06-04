cea8678908f88d53a64033065a66cb0b2bf63957
VlTok <vladimir.tokmeninov@mail.ru>
2023-06-04 16:45:18 +0300

    Проект для распознавания символов

---
+++ src/main/java/com/tokmeninov/projectfirstsenseorgan/ProjectFirstSenseOrganApplication.java
@@ -0,0 +1,13 @@
+package com.tokmeninov.projectfirstsenseorgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFirstSenseOrganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFirstSenseOrganApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tokmeninov/projectfirstsenseorgan/ProjectFirstSenseOrganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokmeninov.projectfirstsenseorgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFirstSenseOrganApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
